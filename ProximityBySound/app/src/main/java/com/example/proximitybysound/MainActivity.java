0d233a535483edb37d14b934afb359666cca5e96
Mathew Nuval <nuval.mathewj@gmail.com>
2019-11-02 16:28:35 -0700

    Added Permission for Audio Recording

--- ProximityBySound/app/src/main/java/com/example/proximitybysound/MainActivity.java
+++ ProximityBySound/app/src/main/java/com/example/proximitybysound/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.proximitybysound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.camera);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission(Manifest.permission.RECORD_AUDIO,REQUEST_RECORD_AUDIO_PERMISSION);
+            }
+        });
+
+    }
+
+    public void checkPermission(String permission, int requestCode) {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{ permission }, requestCode);
+        }
+        else {
+            Toast.makeText(MainActivity.this,"Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
     }
 }
